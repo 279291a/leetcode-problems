1af5186bc33d9561d2ea89010cbbd663cf251deb
王晓丽 <wangxiaoli@troila.com>
2019-08-14 15:36:53 +0800

    合并有序数据-从尾指针开始

--- Array/merge.js
+++ Array/merge.js
@@ -30,5 +30,36 @@ var merge = function(nums1, m, nums2, n) {
     nums1[p++] = nums2[p2++];
   }
 };
+/**
+ * 思路二：从数据结尾开始移动指针
+ */
 
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+const merge2 = function(nums1, m, nums2, n) {
+  if (n < 1) return;
+  if (m < 1) {
+    for (let i = 0; i < n; i++) {
+      nums1[i] = nums2[i];
+    }
+  }
+
+  let p1 = m - 1,
+    p2 = n - 1,
+    p = m + n - 1;
+
+  while (p1 >= 0 && p2 >= 0) {
+    if (nums2[p2] > nums1[p1]) {
+      nums1[p--] = nums2[p2--];
+    } else {
+      nums1[p--] = nums1[p1--];
+    }
+  }
+
+  while (p1 >= 0) {
+    nums1[p--] = nums1[p1--];
+  }
+
+  while (p2 >= 0) {
+    nums1[p--] = nums2[p2--];
+  }
+};
+console.log(merge2([0], 0, [1], 1));
