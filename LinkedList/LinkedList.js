8d60e2f4391afb46f0bd063444d40f220d1c20cb
王晓丽 <wangxiaoli@troila.com>
2020-01-02 13:50:26 +0800

    环形链表1

---
+++ LinkedList/LinkedList.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**快慢指针
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if (!head || !head.next) return false;
+
+  let quick = head.next;
+  let slow = head;
+
+  while (quick !== slow) {
+    if (!fast || !fast.next) return false;
+
+    quick = quick.next.next;
+    slow = slow.next;
+  }
+
+  return true;
+};
