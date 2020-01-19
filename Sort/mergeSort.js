ba8424bc4a0a5ce1fbac55d1b3dae7e21029d5c1
王晓丽 <wangxiaoli@troila.com>
2020-01-19 15:50:36 +0800

    归并排序

--- Sort/mergeSort.js
+++ Sort/mergeSort.js
@@ -1,11 +1,41 @@
-const merge = [];
+const merge = (nums1, nums2) => {
+  let i = 0;
+  let j = 0;
+  const result = [];
 
-const mergeSort = (a, p = 0, q = a.length - 1) => {
-  if (q <= 0) return;
+  while (nums1[i] !== undefined || nums2[j] !== undefined) {
+    if (nums1[i] === undefined) {
+      result.push(nums2[j++]);
+    } else if (nums2[j] === undefined) {
+      result.push(nums1[i++]);
+    } else if (nums1[i] <= nums2[j]) {
+      result.push(nums1[i++]);
+    } else {
+      result.push(nums2[j++]);
+    }
+    console.log(result);
+  }
 
-  const middle = Math.floor((a.length - 1) / 2);
-  const left = mergeSort(a, p, r);
-  mergeSort(a, r + 1, q);
+  return result;
+};
 
-  merge();
+const mergeSort = a => {
+  if (a.length <= 1) return a;
+
+  var tmp;
+
+  if (a.length === 2) {
+    if (a[0] > a[1]) {
+      tmp = a[0];
+      a[0] = a[1];
+      a[1] = tmp;
+    }
+    return a;
+  }
+
+  const middle = Math.floor((a.length + 1) / 2);
+  const left = a.slice(0, middle);
+  const right = a.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
 };
