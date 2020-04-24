2a291f290ebd9f811d26f1d157d18d2789ce7dc9
王晓丽 <wangxiaoli@troila.com>
2020-04-24 17:13:23 +0800

    快速排序增加测试数据

--- Sort/quickSort.js
+++ Sort/quickSort.js
@@ -21,8 +21,6 @@ const partition = (A, p, r) => {
   return i;
 };
 
-var arr = [11, 8, 3, 9, 7, 1, 2, 5];
-
 const quickSort = (A, p = 0, r = A.length - 1) => {
   if (p >= r) return;
 
@@ -30,6 +28,13 @@ const quickSort = (A, p = 0, r = A.length - 1) => {
 
   quickSort(A, p, q - 1);
   quickSort(A, q + 1, r);
+  return A;
 };
+
+const arr = [];
+
+for (let i = 0; i < 1000; i++) {
+  arr.push(Math.floor(Math.random() * 1000));
+}
 
 console.log(quickSort(arr));
