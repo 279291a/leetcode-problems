3ba4c89ee91241a9d1eee8120ab7694fb47fa9b2
王晓丽 <wangxiaoli@troila.com>
2020-04-27 15:27:26 +0800

    查找最后一个等于给定值的元素

---
+++ BinarySearch/findLastElementEqualToTarget.js
@@ -0,0 +1,33 @@
+/**
+ * 给定 有重复元素的数组
+ * 查找第一个值等于给定值的元素
+ */
+
+/**
+ *
+ * @param {number[]} nums
+ * @param {number} value
+ */
+const findLastEqual = (nums, value) => {
+  let low = 0;
+  let high = nums.length - 1;
+  let mid;
+
+  while (low <= high) {
+    mid = Math.floor(high - ((high - low) >> 1));
+
+    if (nums[mid] > value) {
+      high = mid - 1;
+    } else if (nums[mid] < value) {
+      low = mid + 1;
+    } else if (mid === nums.length - 1 || nums[mid + 1] !== value) {
+      return mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(findLastEqual([1, 2, 4, 4, 4, 4, 4, 6, 7, 8, 9], 4));
